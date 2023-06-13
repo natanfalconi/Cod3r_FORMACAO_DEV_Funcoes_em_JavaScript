5b6bf0f89b8e15aecbf0791485ca36352b1e7c75
natanfalconi <nfalconi65@gmail.com>
2023-06-13 16:54:50 -0300

    exemplo map 5

---
+++ array/map_5.js
@@ -0,0 +1,5 @@
+const nums = [9, 8, 7, 6, 5]
+
+nums.map(function (elemento, indice, array) { // o map tem os 3 parametros - não é obrigatório passar os 3 - SEMPRE NESSA ORDEM
+    console.log(elemento, indice, array)
+})
