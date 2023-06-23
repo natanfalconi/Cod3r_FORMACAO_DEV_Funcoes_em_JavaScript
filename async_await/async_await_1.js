43c894bce99059107a87e5c3f9495dc79ad2fffd
natanfalconi <nfalconi65@gmail.com>
2023-06-23 15:46:15 -0300

    exemplo [Pormise] e [Async/Await]

--- async_await/async_await_1.js
+++ async_await/async_await_1.js
@@ -8,20 +8,20 @@ function esperarPor(tempo) {
 
 function executarPromise() {
     esperarPor(2000)
-        .then(() => console.log('2 segundos depois'))
+        .then(() => console.log('[Pormise] 2 segundos depois'))
         .then(() => esperarPor(3000))
-        .then(() => console.log('3 segundos depois'))
+        .then(() => console.log('[Pormise] 3 segundos depois'))
         .then(() => esperarPor(1500))
-        .then(() => console.log('1.5 segundos depois'))
+        .then(() => console.log('[Pormise] 1.5 segundos depois'))
 }
 
 async function executarAsync() {
     await esperarPor(2000)
-    console.log('2 segundos depois')
+    console.log('[Async/Await] 2 segundos depois')
     await esperarPor(3000)
-    console.log('3 segundos depois')
+    console.log('[Async/Await] 3 segundos depois')
     await esperarPor(1500)
-    console.log('1.5 segundos depois')
+    console.log('[Async/Await] 1.5 segundos depois')
 }
 
 
